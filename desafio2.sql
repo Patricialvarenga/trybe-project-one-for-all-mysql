7ea2932bfc3c6e8e6d9fe2bc583fa4b3d409d226
Patricia Silvestre Alvarenga <patriciasilvestrea@gmail.com>
2021-09-29 12:10:48 -0300

    Desafio 2

---
+++ desafio2.sql
@@ -0,0 +1,15 @@
+CREATE VIEW SpotifyClone.estatisticas_musicais AS
+    SELECT 
+        (SELECT 
+                COUNT(*)
+            FROM
+                SpotifyClone.cancoes) AS cancoes,
+        (SELECT 
+                COUNT(*)
+            FROM
+                SpotifyClone.artistas) AS artistas,
+        (SELECT 
+                COUNT(*)
+            FROM
+                SpotifyClone.albuns) AS albuns;
+                
